ff27c1afaabe878b2e0210cffd7290143dd74fc8
이지원 <nowmyturn716@gmail.com>
2026-03-22 13:10:38 +0900

    ch02 adapter file

---
+++ src/hw/ch02/KoreanOutlet.java
@@ -0,0 +1,9 @@
+package hw.ch02;
+
+public class KoreanOutlet {
+    // 한국 표준 전원 : 220V AC
+    public int provide(){
+        System.out.println("한국 표준 전원 : 220V AC");
+        return 220;
+    }
+}
