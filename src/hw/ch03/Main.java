2b8a1e5a10ae563d3355e850952ede35f87dd265
이지원 <nowmyturn716@gmail.com>
2026-03-28 19:47:44 +0900

    ch04 file

--- src/hw/ch03/Main.java
+++ src/hw/ch03/Main.java
@@ -5,11 +5,11 @@ public class Main {
         System.out.println("20221310 이지원");
 
         // [3-1] 기본 테스트
-        AbstractDisplay d1 = new CharDisplay('H');
-        AbstractDisplay d2 = new StringDisplay("Hello");
+        // AbstractDisplay d1 = new CharDisplay('H');
+        // AbstractDisplay d2 = new StringDisplay("Hello");
         
-        d1.display();
-        d2.display();
+        // d1.display();
+        // d2.display();
 
         // [3-2] 반복 횟수 제어
         // AbstractDisplay d1 = new CharDisplay('X', 3);
@@ -19,7 +19,7 @@ public class Main {
         // d2.display();
 
         // [3-3] 새로운 Display
-        // AbstractDisplay num = new NumberDisplay(42, 4);
-        // num.display();
+        AbstractDisplay num = new NumberDisplay(42, 4);
+        num.display();
     }
 }
---
+++ src/hw/ch04/idcard/IDCardFactory.java
@@ -0,0 +1,18 @@
+package hw.ch04.idcard;
+
+
+import hw.ch04.framework.Factory;
+import hw.ch04.framework.Product;
+
+public class IDCardFactory extends Factory {
+    private int serial = 100;
+
+    protected synchronized Product createProduct(String owner) {
+        return new IDCard(owner, serial++);
+    }
+
+    @Override
+    protected void registerProduct(Product product) {
+        System.out.println(product + "을 등록했습니다.");
+    }
+}
