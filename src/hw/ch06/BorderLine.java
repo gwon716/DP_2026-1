701a1142e540fd3428d34afa949556bd31ea58af
이지원 <nowmyturn716@gmail.com>
2026-04-10 00:36:25 +0900

    Assignment Ch06 Prototype Pattern

---
+++ src/hw/ch06/BorderLine.java
@@ -0,0 +1,37 @@
+package hw.ch06;
+
+import hw.ch06.framework.Product;
+
+public class BorderLine implements Product {
+    private char borderChar;
+
+    public BorderLine(char borderChar) {
+        this.borderChar = borderChar;
+    }
+
+    @Override
+    public void use(String s) {
+        int len = s.length();
+        for (int i = 0; i < len + 4; i++) {
+            System.out.print(borderChar);
+        }
+        System.out.println();
+        System.out.println(borderChar + " " + s + " " + borderChar);
+        for (int i = 0; i < len + 4; i++) {
+            System.out.print(borderChar);
+        }
+        System.out.println();
+    }
+
+    @Override
+    public Product createCopy() {
+        Product p = null;
+        try {
+            p = (Product)clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        return p;
+    }
+    
+}
