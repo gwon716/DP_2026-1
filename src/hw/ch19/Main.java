379ecd754d9c20d6ce040b51ed8ebf6482ef449d
이지원 <nowmyturn716@gmail.com>
2026-05-23 21:21:38 +0900

    Assignment Ch19 State

---
+++ src/hw/ch19/Main.java
@@ -0,0 +1,17 @@
+package hw.ch19;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("20221310 이지원");
+        SafeFrame frame = new SafeFrame("State Sample");
+        while (true) {
+            for (int hour = 0; hour < 24; hour++) {
+                frame.setClock(hour);   // 시간 설정 
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                }
+            }
+        }
+    }
+}
